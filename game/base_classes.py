1b00fa02d16008d247e81b6a60f2e93f743eed5b
ThomasMeissnerDS <meissnercorporation@gmx.de>
2022-07-15 15:30:01 +0200

    Changed stones to be class based as well. Each stone will be a separate class holding needed properties.

--- game/base_classes.py
+++ game/base_classes.py
@@ -18,19 +18,47 @@ class Player:
     """
     Player class containing properties of a player.
     :param name: Name of the player (optional).
+    :param unique_id: Unique id of this player.
+    :param player_type: 'human' will require manual imput for this player while
+    'ai' will let artificial intelligence decide on all moves.
     :param stones: List containing all stones a player currently has.
     :param got_30_out: Boolean flagging if the player already placed the first 30 points
     on the board.
     """
     def __init__(self,
                  name: str = 'Thomas',
+                 unique_id: int = 0,
+                 player_type: str = 'ai',
                  turn_active: bool = False,
                  stones=None,
                  got_30_out: bool = False):
         self.name = name
+        self.short_unique_id = unique_id
+        self.full_unique_id = f"player_{unique_id}"
+        self.player_type = player_type
         self.turn_active = turn_active
         self.stones = stones  # expects a list
         self.got_30_out = got_30_out
+
+
+class Stone:
+    """
+    Base class for all stones. Holds properties including current position of each stone.
+    :param unique_id: Integer which will be unique in combination with other properties.
+    """
+    def __init__(self,
+                 unique_id: int = 0,
+                 colour: str = None,
+                 copy_of_colour: int = 0,
+                 is_joker: bool = False,
+                 position: str = None):
+        self.unique_id = unique_id
+        self.short_unique_id = f"{colour}_{unique_id}"
+        self.full_unique_id = f"stone_{colour}_{copy_of_colour}_{unique_id}"
+        self.colour = colour
+        self.copy_of_colour = copy_of_colour
+        self.is_joker = is_joker
+        self.current_position = position
 
 
 class BagOfStones:
@@ -48,6 +76,8 @@ class BagOfStones:
         self.stones_left = stones_left
         self.pieces_per_colour = pieces_per_colour
         self.nb_jokers = nb_jokers
+        self.full_unique_id = "bagofstones"
+        self.stones_objects = {}
 
     def fill_bag(self):
         """
@@ -57,12 +87,23 @@ class BagOfStones:
         self.stones_left = []
         for i in range(self.pieces_per_colour):
             for col in ["red", "blue", "orange", "black"]:
-                stones = [col+f"_{i+1}" for i in range(13)]
-                for piece in stones:
-                    self.stones_left.append(piece)
+                for piece_no in range(13):
+                    stone = Stone(unique_id=piece_no,
+                                  colour=col,
+                                  copy_of_colour=i,
+                                  is_joker=False,
+                                  position="bagofstones")
+                    self.stones_objects[stone.full_unique_id] = stone
+                    self.stones_left.append(stone.full_unique_id)
 
         for i in range(self.nb_jokers):
-            self.stones_left.append("joker")
+            stone = Stone(unique_id=i,
+                          colour="joker",
+                          copy_of_colour=i,
+                          is_joker=True,
+                          position="bagofstones")
+            self.stones_objects[stone.full_unique_id] = stone
+            self.stones_left.append(stone.full_unique_id)
 
     def draw_stone(self, nb_to_draw: int = 1):
         """
